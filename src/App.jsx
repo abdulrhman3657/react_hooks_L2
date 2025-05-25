b0b9449c5d11eecf0060907f1b6dea226369e329
user1 <user01@em/com>
2025-05-25 10:57:11 +0300

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HooksTest from './components/HooksTest'
+
+function App() {
+
+  return (
+    <>
+      <HooksTest/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HooksTest.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function HooksTest() {
+  const [color, setColor] = useState("white ");
+
+  const redbg = () => {
+    setColor("red");
+  }
+
+  const bluebg = () => {
+    setColor("blue");
+  }
+
+  const greenbg = () => {
+    setColor("green");
+  }
+
+  const yellowbg = () => {
+    setColor("yellow");
+  }
+
+  return (
+    <div className="h-screen flex justify-start gap-3 p-10" style={{background:color}}>
+      <button className="border self-start p-3 bg-gray-300 rounded-xl" onClick={() => redbg()}>red</button>
+      <button className="border self-start p-3 bg-gray-300 rounded-xl" onClick={() => bluebg()}>blue</button>
+      <button className="border self-start p-3 bg-gray-300 rounded-xl" onClick={() => greenbg()}>green</button>
+      <button className="border self-start p-3 bg-gray-300 rounded-xl" onClick={() => yellowbg()}>yellow</button>
+    </div>
+  );
+}
+
+export default HooksTest;

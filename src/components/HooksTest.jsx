331c6ed5ce98495d655b5dddd9096f5207b47cd4
user1 <user01@em/com>
2025-05-25 11:06:10 +0300

    improve UI

--- src/components/HooksTest.jsx
+++ src/components/HooksTest.jsx
@@ -21,10 +21,10 @@ function HooksTest() {
 
   return (
     <div className="h-screen flex justify-start gap-3 p-10" style={{background:color}}>
-      <button className="border self-start p-3 bg-gray-300 rounded-xl" onClick={() => redbg()}>red</button>
-      <button className="border self-start p-3 bg-gray-300 rounded-xl" onClick={() => bluebg()}>blue</button>
-      <button className="border self-start p-3 bg-gray-300 rounded-xl" onClick={() => greenbg()}>green</button>
-      <button className="border self-start p-3 bg-gray-300 rounded-xl" onClick={() => yellowbg()}>yellow</button>
+      <button className="border self-start p-3 w-20 bg-gray-300 rounded-xl" onClick={() => redbg()}>red</button>
+      <button className="border self-start p-3 w-20 bg-gray-300 rounded-xl" onClick={() => bluebg()}>blue</button>
+      <button className="border self-start p-3 w-20 bg-gray-300 rounded-xl" onClick={() => greenbg()}>green</button>
+      <button className="border self-start p-3 w-20 bg-gray-300 rounded-xl" onClick={() => yellowbg()}>yellow</button>
     </div>
   );
 }
